03cf1f6194cd9ea838332538bff6ad56b5b2cc08
baca217 <baca217@gmail.com>
2021-04-16 00:32:06 +0000

    added some functionality for trying to print the hit items

--- src/main.rs
+++ src/main.rs
@@ -42,13 +42,68 @@ struct Data
     hits: Vec<Hits>,
 }
 
+fn move_through_menu(stuff: Vec<Hits>)
+{
+    let mut stdout = stdout();
+    enable_raw_mode().unwrap();
+    let MENU = "use left and right arrow keys to move through menu";
+    let MAX = stuff.len();
+    let mut index = 0;
+
+    execute!(stdout, Clear(ClearType::All), cursor::MoveTo(0,0), Print(MENU))
+        .unwrap();
+
+    loop
+    {
+        execute!(stdout, cursor::MoveTo(0,0)).unwrap();
+
+        match read().unwrap()
+        {
+            Event::Key(KeyEvent
+                       {
+                           code : KeyCode::Right,
+                           ..
+                       }) => {
+                if index < MAX
+                {
+                    index += 1;
+                    print_item(&stuff[index])
+                }
+                else
+                {
+                    println!("index is at {}, can't add 1", index);
+                }
+            },
+            Event::Key(KeyEvent
+                       {
+                           code : KeyCode::Left,
+                           ..
+                       }) => {
+                if index > 0
+                {
+                    index -= 1;
+                    print_item(&stuff[index])
+                }
+                else
+                {
+                    println!("index is at {}, can't subtract 1", index);
+                }
+            },
+            _ => execute!(stdout, Clear(ClearType::All), Print(MENU)).unwrap(),
+        }
+    }
+
+    disable_raw_mode().unwrap();
+}
+
+
 fn key_test()
 {
     let mut stdout = stdout();
     enable_raw_mode().unwrap();
+    let MENU = r#"ctrl + q to exit, ctrl + x to print "Hello World", alt + t to print "crossterm is cool""#;
 
-    execute!(stdout, Clear(ClearType::All), cursor::MoveTo(0,0), Print(r#"ctrl + q to exit, ctrl + x 
-        to print "Hello World", alt + t to print "crossterm is cool""#))
+    execute!(stdout, Clear(ClearType::All), cursor::MoveTo(0,0), Print(MENU))
         .unwrap();
 
     loop
@@ -61,31 +116,83 @@ fn key_test()
                        {
                            code : KeyCode::Char('h'),
                            modifiers : KeyModifiers::CONTROL,
-
                        }) => execute!(stdout, Clear(ClearType::All), Print("Hello World!")).unwrap(),
             Event::Key(KeyEvent
                        {
                            code : KeyCode::Char('t'),
                            modifiers : KeyModifiers::ALT,
-
                        }) => execute!(stdout, Clear(ClearType::All), Print("crossterm is cool")).unwrap(),
             Event::Key(KeyEvent
                        {
                            code : KeyCode::Char('q'),
                            modifiers : KeyModifiers::CONTROL,
-
                        }) => break,
-            _ => (),
+            Event::Key(KeyEvent
+                       {
+                           code : KeyCode::Up,
+                           ..
+                       }) => execute!(stdout, Clear(ClearType::All), Print("pressed up!!!")).unwrap(),
+            Event::Key(KeyEvent
+                       {
+                           code : KeyCode::Down,
+                           ..
+                       }) => execute!(stdout, Clear(ClearType::All), Print("pressed down!!!")).unwrap(),
+            Event::Key(KeyEvent
+                       {
+                           code : KeyCode::Right,
+                           ..
+                       }) => execute!(stdout, Clear(ClearType::All), Print("pressed right!!!")).unwrap(),
+            Event::Key(KeyEvent
+                       {
+                           code : KeyCode::Left,
+                           ..
+                       }) => execute!(stdout, Clear(ClearType::All), Print("pressed left!!!")).unwrap(),
+            _ => execute!(stdout, Clear(ClearType::All), Print(MENU)).unwrap(),
         }
     }
 
     disable_raw_mode().unwrap();
 }
 
+fn print_item(i : &Hits)
+{
+    let mut stdout = stdout();
+    enable_raw_mode().unwrap();
+
+    let holder = "";
+    println!(
+    "index : {}s\n
+    type : {}s\n 
+    id : {}s\n 
+    score : {}s\n 
+    item_id : {}s\n 
+    item_name : {}s\n 
+    brand_name : {}s\n 
+    nf_serving_size_qty : {}s\n 
+    nf_serving_size_unit : {}s\n 
+    nf calories : {}s\n 
+    nf total fat : {}s\n",
+    i._index,
+    i._type,
+    i._id,
+    i._score,
+    i.fields.item_id,
+    i.fields.item_name,
+    i.fields.brand_name,
+    i.fields.nf_serving_size_qty,
+    i.fields.nf_serving_size_unit,
+    i.fields.nf_calories,
+    i.fields.nf_total_fat
+    );
+
+    execute!(stdout, Clear(ClearType::All), Print(holder)).unwrap(); 
+}
+
 #[tokio::main]
 async fn main()  -> Result<(), Error>
 {
-    key_test();
+    //key_test();
+    //std::process::exit(1);
     //-------------------------------------------------------------------
     let args: Vec<String> = env::args().collect();
 
@@ -101,20 +208,7 @@ async fn main()  -> Result<(), Error>
         .await?;
 
     let json: Data = res.json().await?;
-    for i in json.hits{
-        println!("index : {}", i._index);
-        println!("type : {}", i._type);
-        println!("id : {}", i._id);
-        println!("score : {}", i._score);
-        println!("item_id : {}", i.fields.item_id);
-        println!("item_name : {}", i.fields.item_name);
-        println!("brand_name : {}", i.fields.brand_name);
-        println!("nf_serving_size_qty : {}", i.fields.nf_serving_size_qty);
-        println!("nf_serving_size_unit : {}", i.fields.nf_serving_size_unit);
-        println!("nf calories : {}", i.fields.nf_calories);
-        println!("nf total fat : {}", i.fields.nf_total_fat);
-        println!("");
-    }
+    move_through_menu(json.hits);
     Ok(())
 }
 //    let string = res.text().await?;
